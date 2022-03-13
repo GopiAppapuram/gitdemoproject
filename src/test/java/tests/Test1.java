7070b90b149c9b9da7484bef1f4a2d1c31ccc732
Gopi Appapuram <68421207+GopiAppapuram@users.noreply.github.com>
2022-03-13 13:28:20 +0530

    Update Test1.java

--- src/test/java/tests/Test1.java
+++ src/test/java/tests/Test1.java
@@ -4,7 +4,7 @@ public class Test1
 {
 	public static void main(String[] args)
 	{
-		String x="ufbukfhbkf#$^&&57$^784FHJ";
+		String x="dhbdahf173e8y1283128312319eywqirr7igerqeiugrqediuqegiuqhdqiudhqduuqwueyqwueyqwueywqeywey31y1y12y127323y217y@&!@T!@!R@!@!t62321683tt12bYufdjfd";
 		String y=x.replaceAll("[^0-9]","");
 		System.out.println(y);
 		System.out.println(x);
