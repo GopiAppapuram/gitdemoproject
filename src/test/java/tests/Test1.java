b7d1ca22bd04cfa05f6287e41821024458eb6d33
kalamgit143 <66198415+kalamgit143@users.noreply.github.com>
2022-03-13 13:25:03 +0530

    Update Test1.java
    changed

--- src/test/java/tests/Test1.java
+++ src/test/java/tests/Test1.java
@@ -4,7 +4,7 @@ public class Test1
 {
 	public static void main(String[] args)
 	{
-		String x="ufbukfhbkf#$^&&57$^784FHJ";
+		String x="ufbukfhbkf#$^&&57$^784FHJAbdulkalam";
 		String y=x.replaceAll("[^0-9]","");
 		System.out.println(y);
 	}
