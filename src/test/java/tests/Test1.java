498d931fc7f3817a877baed25c0d7d7903c6e3e7
Gopi Appapuram <68421207+GopiAppapuram@users.noreply.github.com>
2022-03-13 13:30:43 +0530

    Update Test1.java

--- src/test/java/tests/Test1.java
+++ src/test/java/tests/Test1.java
@@ -5,7 +5,7 @@ public class Test1
 	public static void main(String[] args)
 	{
 		String x="ufbukfhbkf#$^&&57$^784FHJAbdulkalam";
-		String y=x.replaceAll("[^0-9]","");
+		String y=x.replaceAll("[^a-z]","");
 		System.out.println(y);
 	}
 
